1ecf8a9600045ccdbf09698dac4d1fe3eaaa50a1
Ben Fischer <Ben Fischer>
2016-05-14 13:52:36 -0700

    Bugfix for index routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/overall/experience');
+  res.redirect('/overall/0/experience');
 });
 
 module.exports = router;
